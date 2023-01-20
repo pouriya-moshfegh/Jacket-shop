96843f258b17c212b58c23120c8d6439c642b9ae
pouriya moshfegh <pouriya.moshfeghs@gmail.com>
2023-01-21 00:20:35 +0330

    js

---
+++ product.js
@@ -0,0 +1,59 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "white black jacket",
+    price: 110,
+    src: "source/image/3-jacket-png-image.png",
+    material:"cotton"
+  },
+  {
+    id: 2,
+    title: "black leather jacket",
+    price: 128,
+    src: "source/image/black-leather-jacket-png-image.png",
+    material:"leather"
+  },
+  {
+    id: 3,
+    title: "pale green jacket",
+    price: 143,
+    src: "source/image/dark-green-jacket-png-image.png",
+    material:"cotton"
+  },
+  {
+    id: 4,
+    title: "yellow autumn jacket",
+    price: 138,
+    src: "source/image/green-jacket-png-image.png",
+    material:"cotton"
+  },
+  {
+    id: 5,
+    title: "lite brown jacket",
+    price: 125,
+    src: "source/image/hoodi-jacket-png-image.png",
+    material:"silk"
+  },
+  {
+    id: 6,
+    title: "black long jacket",
+    price: 112,
+    src: "source/image/jacket-png-image.png",
+    material:"silk"
+  },
+  {
+    id: 7,
+    title: "Dark leather jacket",
+    price: 156,
+    src: "source/image/leather-jacket-girl-free-clipart-hq.png",
+    material:"leather"
+  },
+  {
+    id: 8,
+    title: "black leather jacket",
+    price: 173,
+    src: "source/image/leather-jacket-png-image.png",
+    material:"leather"
+  },
+];
+
